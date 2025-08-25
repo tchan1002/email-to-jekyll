ccb04eb635301f7b86acd17416dd25b534534ad1
tchan1002 <tchan0@uchicago.edu>
2025-08-25 16:05:23 +0800

    Update email.js. tag general for coco

--- api/email.js
+++ api/email.js
@@ -89,6 +89,7 @@ function makeFrontMatter({ title, dateISO, categories = [] }) {
     "layout: post",
     `title: "${safeTitle}"`,
     `date: ${dateISO} ${TZ}`,
+    `tag: "general"`,
   ];
   if (categories && categories.length) {
     lines.push(`categories: [${categories.map((c) => `"${c}"`).join(", ")}]`);
